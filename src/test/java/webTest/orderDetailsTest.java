b5feef987c85e66f22c82dd7e286ec57abb19132
dina <dina.taher@hotmail.com>
2023-09-25 20:55:45 +0300

    commit test 2

--- src/test/java/webTest/orderDetailsTest.java
+++ src/test/java/webTest/orderDetailsTest.java
@@ -1,28 +1,33 @@
 package webTest;
 
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
 import mobilePage.CheckoutPage;
-import org.openqa.selenium.WebDriver;
+import mobilePage.TrackOrderPage;
+import mobileTest.MobileTestBase;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import webPage.orderDetailsPage;
+import webPage.OrderDetailsWebPage;
 
-import static org.testng.AssertJUnit.assertEquals;
+public class OrderDetailsTest extends WebTestBase {
 
-public class orderDetailsTest extends WebTestBase{
+    OrderDetailsWebPage orderDetailsWebPage;
+    CheckoutPage checkoutPage;
+    MobileTestBase mobileTestBase;
 
-    orderDetailsPage orderDetailsPage;
-
+    TrackOrderPage trackOrderPage;
 
     @Test
-    public void checkOrderDetails() throws InterruptedException {
-        orderDetailsPage=new orderDetailsPage(driver);
-        orderDetailsPage.getHandle(driver);
-        orderDetailsPage.getDeliveryFeeForUser(driver);
-       Assert.assertEquals(orderDetailsPage.DeliverFeeForUser, "30");
-        orderDetailsPage.getFinalTotalPrice(driver);
-        Assert.assertEquals(orderDetailsPage.FinalTotal, "30");
-    }
+    public void assertOnOrderDetails() {
+        orderDetailsWebPage = new OrderDetailsWebPage(driver);
+        mobileTestBase = new MobileTestBase();
+        checkoutPage = new CheckoutPage(MobileTestBase.driver);
+        trackOrderPage = new TrackOrderPage(MobileTestBase.driver);
+        String deliveryFee = orderDetailsWebPage.getDeliveryFeeUser(driver);
+        String totalPrice = orderDetailsWebPage.getFinalTotal(driver);
 
+        Assert.assertEquals(orderDetailsWebPage.getOrderNumber(driver), "Order Number: " + trackOrderPage.OrderNumber);
+        // Transform the Actual Result String to int , Get The Correct Number Only From expected String "10.0 SAR"
+        Assert.assertEquals(Integer.parseInt(deliveryFee),Integer.parseInt(checkoutPage.DeliveryFee.split("\\.")[0]));
+        Assert.assertEquals(totalPrice+".0 SAR",(checkoutPage.TotalAmount));
+
+    }
 }
